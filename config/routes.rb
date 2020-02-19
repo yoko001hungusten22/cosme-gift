9715580f717eb1c3f31dd4d75da9ecb32e8b392f
Natsumi-rino <himegoto12@gmail.com>
2020-02-19 12:24:11 +0900

    定番ランキングページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'main#index'
   get  '/index/:year/:event',    to: 'main#index'
   get  '/gift',    to: 'main#gift'
+  get  '/gift_rank',   to: 'main#gift_rank'
 end
