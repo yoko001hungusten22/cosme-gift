0b4f30191b6b58d6528d935f2e522b2840545c2b
2754 <fkesys@gmail.com>
2020-02-19 05:29:04 +0000

    いろいろ修正

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,12 @@ class MainController < ApplicationController
   def index
   end
 
-  def gift
+  def gift_new
   end
 
   def gift_rank
+  end
+  
+  def gift_recommend
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'main#index'
-  get  '/index/:year/:event',    to: 'main#index'
-  get  '/gift',    to: 'main#gift'
-  get  '/gift_rank',   to: 'main#gift_rank'
+  get  '/gift/new',        to: 'main#gift_new'
+  get  '/gift/rank',       to: 'main#gift_rank'
+  get  '/gift/new',        to: 'main#gift_new'
+  get  '/gift/recommend',  to: 'main#gift_recommend'
 end
