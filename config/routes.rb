bcd2bb1e5fc9aae5bcb9dad4918b7235d5f3d841
2754 <fkesys@gmail.com>
2020-02-19 06:44:12 +0000

    イベント月の項目にリンク付与(リンク先のページはまだない)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get  '/gift/rank',       to: 'main#gift_rank'
   get  '/gift/recommend',  to: 'main#gift_recommend'
   
-  get  '/gift/:year/:event',        to: 'main#gift_event'
-  get  '/gift/:year/:month',        to: 'main#gift_month'
+  get  '/gift/:year/:event',  to: 'main#gift_event', as: :gift_event
+  get  '/gift/:year/:month',  to: 'main#gift_month', as: :gift_month
 end
