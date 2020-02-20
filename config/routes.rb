2466efd25f6bc66ff6ba1708bea5d43a15f82d76
2754 <fkesys@gmail.com>
2020-02-20 06:22:37 +0000

    トップページ9割完成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   get  '/gift/rank',       to: 'main#gift_rank'
   get  '/gift/recommend',  to: 'main#gift_recommend'
   
-  get  '/gift/:year/:event',  to: 'main#gift_event', as: :gift_event
-  get  '/gift/:year/:month',  to: 'main#gift_month', as: :gift_month
+  get  '/index/:event_name',  to: 'main#index', as: :index_event
 end
